e0c10a96b4e9c8048576346353640b27397ef8ae
Cygnus-x1br <jeanmarcel83@gmail.com>
2024-05-11 14:55:30 -0300

    inclusoes tabelas sql

--- database/natacao.sql
+++ database/natacao.sql
@@ -1,16 +1,12 @@
 CREATE DATABASE natacao;
 USE natacao;
+-- Criacao de Tabelas 
 CREATE TABLE tb_equipe (
   IDEQUIPE INT PRIMARY KEY AUTO_INCREMENT,
   nomeEquipe varchar(100) NOT NULL,
   nomeFantasiaEquipe varchar(50),
   logoEquipe varchar(150),
   ID_FEDERACAO INT
-);
-CREATE TABLE tb_estilo (
-  IDESTILO INT PRIMARY KEY AUTO_INCREMENT,
-  nomeEstilo varchar(100) NOT NULL,
-  distanciaEstilo INT NOT NULL
 );
 CREATE TABLE tb_prova (
   IDPROVA INT PRIMARY KEY AUTO_INCREMENT,
@@ -36,11 +32,6 @@ CREATE TABLE tb_atleta (
   rgAtleta VARCHAR(12),
   ID_TORNEIO INT
 );
-CREATE TABLE tb_estado (
-  IDESTADO INT PRIMARY KEY AUTO_INCREMENT,
-  nomeEstado varchar(30) NOT NULL,
-  siglaEstado char(2) NOT NULL
-);
 CREATE TABLE tb_federacao (
   IDFEDERACAO INT PRIMARY KEY AUTO_INCREMENT,
   nomeFederacao varchar(100) NOT NULL,
@@ -48,20 +39,55 @@ CREATE TABLE tb_federacao (
   logoFederacao varchar(150),
   ID_ESTADO INT NOT NULL
 );
-CREATE TABLE tb_piscina (
-  IDPISCINA INT PRIMARY KEY AUTO_INCREMENT,
-  tamanhoPiscina INT NOT NULL
-);
 CREATE TABLE tb_indices (
   IDINDICE INT PRIMARY KEY AUTO_INCREMENT,
   anoIndice INT NOT NULL,
   tempoIndice TIME(2) NOT NULL,
-  categoriaIndice VARCHAR(30) NOT NULL,
+  ID_CATEGORIA INT NOT NULL,
   generoIndice ENUM('M', 'F') NOT NULL,
   tipoIndice VARCHAR(30) NOT NULL,
   ID_ESTILO INT NOT NULL,
   ID_PISCINA INT NOT NULL
 );
+CREATE TABLE tb_tempoAtleta (
+  IDTMPATLETA INT PRIMARY KEY AUTO_INCREMENT,
+  tempoAtleta TIME(2) NOT NULL,
+  ID_PROVA INT NOT NULL,
+  ID_ATLETA INT NOT NULL
+);
+CREATE TABLE tb_metaAtleta (
+  IDMETAATLETA INT PRIMARY KEY AUTO_INCREMENT,
+  tempoMetaAtleta TIME(2) NOT NULL,
+  ID_TAMPISCINA INT NOT NULL,
+  ID_ESTILO INT NOT NULL,
+  ID_ATLETA INT NOT NULL
+);
+-- Criacao de tabelas fixas
+CREATE TABLE tb_categoria (
+  IDCATEGORIA INT PRIMARY KEY AUTO_INCREMENT,
+  nomeCategoria varchar(100) NOT NULL,
+  idadeCategoria INT
+);
+CREATE TABLE tb_piscina (
+  IDPISCINA INT PRIMARY KEY AUTO_INCREMENT,
+  tamanhoPiscina INT NOT NULL
+);
+CREATE TABLE tb_distancia (
+  IDDISTANCIA INT PRIMARY KEY AUTO_INCREMENT,
+  distancia INT NOT NULL
+);
+CREATE TABLE tb_estilo (
+  IDESTILO INT PRIMARY KEY AUTO_INCREMENT,
+  nomeEstilo varchar(100) NOT NULL,
+  ID_DISTANCIA INT NOT NULL
+);
+CREATE TABLE tb_estado (
+  IDESTADO INT PRIMARY KEY AUTO_INCREMENT,
+  nomeEstado varchar(30) NOT NULL,
+  siglaEstado char(2) NOT NULL
+);
+-- Insercao de dados nas tabelas fixas
+-- Tabla Estados
 INSERT INTO tb_estado
 VALUES(null, 'Acre', 'AC');
 INSERT INTO tb_estado
@@ -116,9 +142,51 @@ INSERT INTO tb_estado
 VALUES(null, 'Santa Catarina', 'SC');
 INSERT INTO tb_estado
 VALUES(null, 'Rio Grande do Sul', 'RS');
+-- Tabela tamanho piscina
 INSERT INTO tb_piscina
 VALUES(null, 25);
 INSERT INTO tb_piscina
 VALUES(null, 50);
 INSERT INTO tb_piscina
 VALUES(null, 00);
+.-- Tabela Estilos
+INSERT INTO tb_estilo
+VALUES(null, 'Livre');
+INSERT INTO tb_estilo
+VALUES(null, 'Borboleta');
+INSERT INTO tb_estilo
+VALUES(null, 'Costas');
+INSERT INTO tb_estilo
+VALUES(null, 'Peito');
+INSERT INTO tb_estilo
+VALUES(null, 'Medley');
+-- Tabela Distancias Provas
+INSERT INTO tb_distancia
+VALUES(null, 25);
+INSERT INTO tb_distancia
+VALUES(null, 50);
+INSERT INTO tb_distancia
+VALUES(null, 100);
+INSERT INTO tb_distancia
+VALUES(null, 200);
+INSERT INTO tb_distancia
+VALUES(null, 400);
+INSERT INTO tb_distancia
+VALUES(null, 800);
+INSERT INTO tb_distancia
+VALUES(null, 1500);
+-- Tabela Categorias
+INSERT INTO tb_categoria
+VALUES(null, 'Pre-Mirim', 7);
+INSERT INTO tb_categoria
+VALUES(null, 'Pre-Mirim', 8);
+INSERT INTO tb_categoria
+VALUES(null, 'Mirim 1', 9);
+INSERT INTO tb_categoria
+VALUES(null, 'Mirim 2', 10);
+INSERT INTO tb_categoria
+VALUES(null, 'Petiz 1', 11);
+INSERT INTO tb_categoria
+VALUES(null, 'Petiz 1', 11);
+INSERT INTO tb_categoria
+VALUES(null, 'Adulto', 99);
