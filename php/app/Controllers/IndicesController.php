2bb69a1b24c47a4ff55b4f8deed14d290e80a05c
Cygnus-x1br <jeanmarcel83@gmail.com>
2024-05-30 07:15:51 -0300

    ajustes em estilo das paginas

--- php/app/Controllers/IndicesController.php
+++ php/app/Controllers/IndicesController.php
@@ -7,57 +7,67 @@ use MF\Model\Container;
 
 class IndicesController extends Action
 {
-  public function list_indices()
-  {
-    $indices = Container::getModel('Indices');
-    $indices_data = $indices->getIndicesSorted();
-    $this->viewData->indices = $indices_data;
+    public function list_indices()
+    {
+        $indices = Container::getModel('Indices');
+        $indices_data = $indices->getIndicesSorted();
+        $this->viewData->indices = $indices_data;
 
-    $this->viewData->anosIndice = $this->list_anos();
-    $this->viewData->tipoIndice = $this->list_tipos();
-    $this->viewData->categorias = $this->list_categorias();
+        $this->viewData->anosIndice = $this->list_anos();
+        $this->viewData->tipoIndice = $this->list_tipos();
+        $this->viewData->categorias = $this->list_categorias();
+        $this->viewData->piscinas = $this->list_piscinas();
 
-    $this->render('list_indices');
-  }
-  public function list_anos()
-  {
-    $indices = Container::getModel('Indices');
-    $indices_data = $indices->getIndicesSorted();
-    foreach ($indices_data as $key => $value) {
-      $anos[] = $value['anoIndice'];
+        $this->render('list_indices');
     }
-    return array_unique($anos);
-  }
-  public function list_tipos()
-  {
-    $indices = Container::getModel('Indices');
-    $indices_data = $indices->getIndicesSorted();
-    foreach ($indices_data as $key => $value) {
-      $tipoIndice[] = $value['tipoIndice'];
+    public function list_anos()
+    {
+        $indices = Container::getModel('Indices');
+        $indices_data = $indices->getIndicesSorted();
+        foreach ($indices_data as $key => $value) {
+            $anos[] = $value['anoIndice'];
+        }
+        return array_unique($anos);
     }
-    return array_unique($tipoIndice);
-  }
-  public function list_categorias()
-  {
-    $categorias = Container::getModel('Categoria');
-    $categorias_data = $categorias->getAllCategorias();
-    return $categorias_data;
-  }
+    public function list_tipos()
+    {
+        $indices = Container::getModel('Indices');
+        $indices_data = $indices->getIndicesSorted();
+        foreach ($indices_data as $key => $value) {
+            $tipoIndice[] = $value['tipoIndice'];
+        }
+        return array_unique($tipoIndice);
+    }
+    public function list_categorias()
+    {
+        $categorias = Container::getModel('Categoria');
+        $categorias_data = $categorias->getAllCategorias();
+        return $categorias_data;
+    }
+    public function list_piscinas()
+    {
+        $piscinas = Container::getModel('Piscina');
+        $piscinas_data = $piscinas->getPiscinas();
+        foreach ($piscinas_data as $key => $value) {
+            $tamanhos[] = $value['tamanhoPiscina'];
+        }
+        return array_unique($tamanhos);
+    }
 
-  public function filtra_indices()
-  {
-    print_r($_POST);
-    $indices = Container::getModel('Indices');
-    $indices->__set('anoIndice', $_POST['anoIndice']);
-    $indices->__set('tipoIndice', $_POST['tipoIndice']);
-    $indices->__set('generoIndice', $_POST['generoIndice']);
-    $indices->__set('id_categoria', $_POST['id_categoria']);
-    $indices_data = $indices->getIndicesFiltered();
-    $this->viewData->indices = $indices_data;
+    public function filtra_indices()
+    {
+        $indices = Container::getModel('Indices');
+        $indices->__set('anoIndice', $_POST['anoIndice']);
+        $indices->__set('tipoIndice', $_POST['tipoIndice']);
+        $indices->__set('generoIndice', $_POST['generoIndice']);
+        $indices->__set('id_categoria', $_POST['id_categoria']);
+        $indices_data = $indices->getIndicesFiltered();
+        $this->viewData->indices = $indices_data;
 
-    $this->viewData->anosIndice = $this->list_anos();
-    $this->viewData->tipoIndice = $this->list_tipos();
-    $this->viewData->categorias = $this->list_categorias();
-    $this->render('list_indices');
-  }
+        $this->viewData->anosIndice = $this->list_anos();
+        $this->viewData->tipoIndice = $this->list_tipos();
+        $this->viewData->categorias = $this->list_categorias();
+        $this->viewData->piscinas = $this->list_piscinas();
+        $this->render('list_indices');
+    }
 }
--- php/app/Models/Indices.php
+++ php/app/Models/Indices.php
@@ -6,51 +6,51 @@ use MF\Model\Model;
 
 class Indices extends Model
 {
-  private $idindice;
-  private $anoIndice;
-  private $tempoIndice;
-  private $id_categoria;
-  private $generoIndice;
-  private $tipoIndice;
-  private $id_distanciaestlo;
-  private $id_piscina;
+    private $idindice;
+    private $anoIndice;
+    private $tempoIndice;
+    private $id_categoria;
+    private $generoIndice;
+    private $tipoIndice;
+    private $id_distanciaestlo;
+    private $id_piscina;
 
-  public function __get($atribute)
-  {
-    return $this->$atribute;
-  }
+    public function __get($atribute)
+    {
+        return $this->$atribute;
+    }
 
-  public function __set($atribute, $value)
-  {
-    $this->$atribute = $value;
-  }
+    public function __set($atribute, $value)
+    {
+        $this->$atribute = $value;
+    }
 
-  public function getAllIndices()
-  {
-    $categoria = "SELECT * FROM tb_indices ORDER BY IDINDICE ASC";
-    $stmt = $this->db->prepare($categoria);
-    $stmt->execute();
+    public function getAllIndices()
+    {
+        $categoria = "SELECT * FROM tb_indices ORDER BY IDINDICE ASC";
+        $stmt = $this->db->prepare($categoria);
+        $stmt->execute();
 
-    return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-  }
-  public function getIndicesSorted()
-  {
-    $categoria = "SELECT anoIndice, tempoIndice, generoIndice, tipoIndice, ID_CATEGORIA, p.tamanhoPiscina, c.nomeCategoria, d.distancia, e.nomeEstilo FROM tb_indices
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+    public function getIndicesSorted()
+    {
+        $categoria = "SELECT anoIndice, tempoIndice, generoIndice, tipoIndice, ID_CATEGORIA, p.tamanhoPiscina, c.nomeCategoria, d.distancia, e.nomeEstilo FROM tb_indices
      INNER JOIN tb_categoria AS c ON ID_CATEGORIA = c.IDCATEGORIA
      INNER JOIN tb_distancia AS d ON 
      (SELECT ID_DISTANCIA AS dist FROM tba_distancia_estilo WHERE tb_indices.ID_DISTANCIAESTILO = IDDISTANCIAESTILO) = d.IDDISTANCIA
      INNER JOIN tb_estilo AS e ON 
      (SELECT ID_ESTILO AS est FROM tba_distancia_estilo WHERE tb_indices.ID_DISTANCIAESTILO = IDDISTANCIAESTILO) = e.IDESTILO
      INNER JOIN tb_piscina AS p ON ID_PISCINA = p.IDPISCINA 
-     ORDER BY generoIndice DESC, ID_DISTANCIAESTILO ASC, anoIndice DESC;;";
-    $stmt = $this->db->prepare($categoria);
-    $stmt->execute();
+     ORDER BY ID_CATEGORIA ASC, ID_DISTANCIAESTILO ASC, anoIndice DESC;;";
+        $stmt = $this->db->prepare($categoria);
+        $stmt->execute();
 
-    return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-  }
-  public function getIndicesFiltered()
-  {
-    $indices = "SELECT anoIndice, tempoIndice, generoIndice, tipoIndice, ID_CATEGORIA, p.tamanhoPiscina, c.nomeCategoria, d.distancia, e.nomeEstilo FROM tb_indices
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+    public function getIndicesFiltered()
+    {
+        $indices = "SELECT anoIndice, tempoIndice, generoIndice, tipoIndice, ID_CATEGORIA, p.tamanhoPiscina, c.nomeCategoria, d.distancia, e.nomeEstilo FROM tb_indices
      INNER JOIN tb_categoria AS c ON ID_CATEGORIA = c.IDCATEGORIA
      INNER JOIN tb_distancia AS d ON 
      (SELECT ID_DISTANCIA AS dist FROM tba_distancia_estilo WHERE tb_indices.ID_DISTANCIAESTILO = IDDISTANCIAESTILO) = d.IDDISTANCIA
@@ -58,37 +58,37 @@ class Indices extends Model
      (SELECT ID_ESTILO AS est FROM tba_distancia_estilo WHERE tb_indices.ID_DISTANCIAESTILO = IDDISTANCIAESTILO) = e.IDESTILO
      INNER JOIN tb_piscina AS p ON ID_PISCINA = p.IDPISCINA
      WHERE ";
-    if (!empty($this->__get('anoIndice'))) {
-      $indices .= "anoIndice = :anoIndice AND ";
-    }
-    if (!empty($this->__get('generoIndice'))) {
-      $indices .= "generoIndice = :generoIndice AND ";
-    }
-    if (!empty($this->__get('tipoIndice'))) {
-      $indices .= "tipoIndice = :tipoIndice AND ";
-    }
-    if (!empty($this->__get('id_categoria'))) {
-      $indices .= "ID_CATEGORIA = :categoria AND ";
-    }
-    $indices .= "1 = 1 ";
-    $indices .= "ORDER BY generoIndice DESC, ID_DISTANCIAESTILO ASC, anoIndice DESC;";
+        if (!empty($this->__get('anoIndice'))) {
+            $indices .= "anoIndice = :anoIndice AND ";
+        }
+        if (!empty($this->__get('generoIndice'))) {
+            $indices .= "generoIndice = :generoIndice AND ";
+        }
+        if (!empty($this->__get('tipoIndice'))) {
+            $indices .= "tipoIndice = :tipoIndice AND ";
+        }
+        if (!empty($this->__get('id_categoria'))) {
+            $indices .= "ID_CATEGORIA = :categoria AND ";
+        }
+        $indices .= "1 = 1 ";
+        $indices .= "ORDER BY generoIndice DESC, ID_DISTANCIAESTILO ASC, anoIndice DESC;";
 
-    $stmt = $this->db->prepare($indices);
-    if (!empty($this->__get('anoIndice'))) {
-      $stmt->bindValue(':anoIndice', $this->__get('anoIndice'));
-    }
-    if (!empty($this->__get('generoIndice'))) {
-      $stmt->bindValue(':generoIndice', $this->__get('generoIndice'));
-    }
-    if (!empty($this->__get('tipoIndice'))) {
-      $stmt->bindValue(':tipoIndice', $this->__get('tipoIndice'));
-    }
-    if (!empty($this->__get('id_categoria'))) {
-      $stmt->bindValue(':categoria', $this->__get('id_categoria'));
-    }
+        $stmt = $this->db->prepare($indices);
+        if (!empty($this->__get('anoIndice'))) {
+            $stmt->bindValue(':anoIndice', $this->__get('anoIndice'));
+        }
+        if (!empty($this->__get('generoIndice'))) {
+            $stmt->bindValue(':generoIndice', $this->__get('generoIndice'));
+        }
+        if (!empty($this->__get('tipoIndice'))) {
+            $stmt->bindValue(':tipoIndice', $this->__get('tipoIndice'));
+        }
+        if (!empty($this->__get('id_categoria'))) {
+            $stmt->bindValue(':categoria', $this->__get('id_categoria'));
+        }
 
-    $stmt->execute();
+        $stmt->execute();
 
-    return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-  }
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
 }
