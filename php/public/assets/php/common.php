4703c8a576c1507aa23aac43162af286ced7753e
Cygnus-x1br <jeanmarcel83@gmail.com>
2024-08-02 17:34:53 -0300

    Incluido calculo diferenca tempo

--- php/public/assets/php/common.php
+++ php/public/assets/php/common.php
@@ -19,6 +19,33 @@ function tempoEmSegundos($tempo)
   return $tempoEmSegundos;
 }
 
+function tempoEmMinutos($tempo)
+{
+    $signal = '';
+    if($tempo < -60) {
+        $signal = '-';
+        $tempo = abs($tempo);
+    }
+    if($tempo > 60) {
+        $tempoEmMinutos = floor($tempo / 60);
+        $tempoEmSegundos = $tempo - ($tempoEmMinutos * 60);
+        if($tempoEmSegundos < 9) {
+            $tempoEmSegundos = '0'. $tempoEmSegundos;
+        }
+        if($tempoEmMinutos < 9 ) {
+            $tempoEmMinutos = '0' . $tempoEmMinutos;
+        }
+        $tempoConvertido = $tempoEmMinutos . ':' . $tempoEmSegundos;
+    } else {
+        $tempoConvertido = $tempo;
+    }
+    //echo $tempoEmMinutos . '<br>';
+    //echo $tempoEmSegundos . '<br>';
+    //echo $tempoConvertido . '<br>';
+    return $signal . $tempoConvertido;
+    
+}
+
 function indiceTecnico($tempo, $id_distanciaestilo, $indicesMundial, $genero)
 {
     $tempoAtleta = tempoEmSegundos($tempo);
@@ -32,4 +59,15 @@ function indiceTecnico($tempo, $id_distanciaestilo, $indicesMundial, $genero)
     $indiceTecnico = 1000 * pow(($tempoIndice / $tempoAtleta), 3);
 
     return floor($indiceTecnico);
+}
+
+function dd($var) {
+    var_dump($var);
+}
+
+function print_var($var) {
+    echo '<pre>';
+    print_r($var);
+    echo '</pre>';
+    
 }
